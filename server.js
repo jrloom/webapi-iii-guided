0a18f748ec1f2162b6a8a242ea1b0a4d41af69c1
Jason Loomis <jason@jrloomis.com>
2019-10-30 13:31:41 -0400

    password

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express"); // importing a CommonJS module
 const helmet = require("helmet"); // import third party middleware
+const morgan = require("morgan");
 
 const hubsRouter = require("./hubs/hubs-router.js");
 
@@ -7,21 +8,35 @@ const server = express();
 
 // write custom middleware
 // function dateLogger(req, res, next) {}
-const dateLogger = (req, res, next) => {
-  console.log(new Date().toISOString());
-  next();
-};
+// const dateLogger = (req, res, next) => {
+// console.log(new Date().toISOString());
+// next();
+// };
+//
+// function logger(req, res, next) {
+// console.log(`${req.method} to ${req.url} from ${req.get("Origin")}`);
+// next();
+// }
 
-function logger(req, res, next) {
-  console.log(`${req.method} to ${req.url} from ${req.get("Origin")}`);
-  next();
+function gateKeeper(req, res, next) {
+  const password = req.headers.password || "";
+  if (password.toLowerCase() === "mellon") {
+    next();
+  } else if (password === "") {
+    res.status(400).json({ message: "provide a password" });
+  } else {
+    res.status(401).json({ message: "you shall not pass" });
+  }
 }
 
 // global middleware
+
 server.use(helmet()); // invoke third party middleware (with defaults)
+server.use(morgan("dev"));
 server.use(express.json()); // built-in middleware
-server.use(dateLogger); // use custom middleware
-server.use(logger);
+server.use(gateKeeper);
+// server.use(dateLogger); // use custom middleware
+// server.use(logger);
 
 server.use("/api/hubs", hubsRouter);
 
