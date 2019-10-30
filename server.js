73456aee698549fc3f3087c21d8459655862fd89
Jason Loomis <jason@jrloomis.com>
2019-10-30 13:03:33 -0400

    logger middleware

--- server.js
+++ server.js
@@ -1,15 +1,32 @@
-const express = require('express'); // importing a CommonJS module
+const express = require("express"); // importing a CommonJS module
+const helmet = require("helmet"); // import third party middleware
 
-const hubsRouter = require('./hubs/hubs-router.js');
+const hubsRouter = require("./hubs/hubs-router.js");
 
 const server = express();
 
-server.use(express.json());
+// write custom middleware
+// function dateLogger(req, res, next) {}
+const dateLogger = (req, res, next) => {
+  console.log(new Date().toISOString());
+  next();
+};
 
-server.use('/api/hubs', hubsRouter);
+function logger(req, res, next) {
+  console.log(`${req.method} to ${req.url} from ${req.get("Origin")}`);
+  next();
+}
 
-server.get('/', (req, res) => {
-  const nameInsert = (req.name) ? ` ${req.name}` : '';
+// global middleware
+server.use(helmet()); // invoke third party middleware (with defaults)
+server.use(express.json()); // built-in middleware
+server.use(dateLogger); // use custom middleware
+server.use(logger);
+
+server.use("/api/hubs", hubsRouter);
+
+server.get("/", (req, res) => {
+  const nameInsert = req.name ? ` ${req.name}` : "";
 
   res.send(`
     <h2>Lambda Hubs API</h2>
